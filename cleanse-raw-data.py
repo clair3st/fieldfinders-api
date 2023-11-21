a25d05bb05add001775c7deb1cf74df49b6956ff
clair3st <clairejgatenby@gmail.com>
2023-11-20 18:30:36 -0800

    cleaned data and created json file

--- cleanse-raw-data.py
+++ cleanse-raw-data.py
@@ -11,16 +11,21 @@ jsonFilePath = r'park_features.json'
 
 start = time.perf_counter()
 
-jsonArray = []
-
 df = pd.read_csv(csvFilePath)
 
-print(df.iloc[0])
- 
-print("---------------")
+# Remove unwanted columns from the data
+df = df.drop(columns=["PMAID", "Alt_Name", "Feature_ID", "CHILD_DESC","YOUTH_ONLY"])
 
+# Filter data by sporting features 
+filtered_df = df[df["Feature_Desc"].str.contains("Tennis|Soccer|Ball|rugby|pool|criket|boat|skate|track|golf", case=False)]
+
+# Convert to json
+jsonData = filtered_df.to_json(orient='records')
+
+with open(jsonFilePath, 'w', encoding='utf-8') as f:
+	json.dump(jsonData, f, ensure_ascii=False, indent=4)
 
 finish = time.perf_counter()
 
 
-print(f"Converted csv raw data to json successfully in {finish - start:0.4f} seconds")
+print(f"Converted csv raw data to {len(filtered_df.index)} json rows successfully in {finish - start:0.4f} seconds")
