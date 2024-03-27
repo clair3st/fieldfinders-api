488723f9827635f4e6bf15d022db025cbcf64bf8
clair3st <clairejgatenby@gmail.com>
2024-03-27 13:30:45 -0700

    adding api route to return all park features

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # Importing flask and creating a flask application get 
 import flask
+import json
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -7,5 +9,11 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
 	return "Field Finders. This site is a prototype API for park facility data."
+
+@app.route('/api/v1/resources/features/all', methods=['GET'])
+def api_all():
+	with app.open_resource('park_features.json') as f:
+		data = json.load(f)
+	return jsonify(data)
 
 app.run()
